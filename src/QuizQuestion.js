7b3018cfc8dc77b7f241927dc2c9888d9a27f5b1
Emmanuel <eherdezmoran@gmail.com>
2019-06-03 22:50:42 -0500

    Error messages. Added error messages to the QuizQuestion Component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,18 @@ import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {incorrectAnswer : false};
+
+    }
+
     handleClick(button_text){
         if(button_text === this.props.quiz_question.answer  ){
             this.props.showNextQuestionHandler();
+            this.setState({incorrectAnswer : false});
+        }else{
+            this.setState({incorrectAnswer : true});
         }
     }
 
@@ -26,6 +35,7 @@ class QuizQuestion extends Component{
                     
                     </ul>
                 </section>
+                { this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null }
             </main>
         );
     }
